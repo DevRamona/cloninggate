d538a0708369051ce2d5c72bf91aed531429d344
DevRamona <r.ingabire@alustudent.com>
2024-05-17 15:21:24 +0200

    additional changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,29 @@ module.exports = {
   theme: {
     
     
-    container: {
-      center: false,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        md: "4rem",
-        lg: "6rem",
-        xl: "8rem",
-        "2xl": "12rem",
-        "3xl": "16rem",
-        "4xl": "20rem",
-      },
-      margin: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        md: "4rem",
-        lg: "6rem",
-        xl: "8rem",
-        "2xl": "12rem",
-        "3xl": "16rem",
-        "4xl": "20rem",
-      },
-    },
+    // container: {
+    //   center: false,
+    //   padding: {
+    //     DEFAULT: "1rem",
+    //     sm: "2rem",
+    //     md: "4rem",
+    //     lg: "6rem",
+    //     xl: "8rem",
+    //     "2xl": "12rem",
+    //     "3xl": "16rem",
+    //     "4xl": "20rem",
+    //   },
+    //   margin: {
+    //     DEFAULT: "1rem",
+    //     sm: "2rem",
+    //     md: "4rem",
+    //     lg: "6rem",
+    //     xl: "8rem",
+    //     "2xl": "12rem",
+    //     "3xl": "16rem",
+    //     "4xl": "20rem",
+    //   },
+    // },
     extend: {
       colors: {
         primary:'#d5d5d5', 
