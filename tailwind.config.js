236bbcd6ed92bb324fd04d5e5885717f7d2f2113
DevRamona <r.ingabire@alustudent.com>
2024-05-17 08:49:01 +0200

    added a predefined class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,29 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    container: {
+      center: false,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+      margin: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+    },
     extend: {},
   },
   plugins: [],
